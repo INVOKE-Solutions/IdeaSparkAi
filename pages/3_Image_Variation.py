5acada98a4ea90be496eac5bbc7b321ac6b3c7b5
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-08-18 13:16:47 +0800

    Update 3_Image_Variation.py

--- pages/3_Image_Variation.py
+++ pages/3_Image_Variation.py
@@ -19,7 +19,7 @@ st.set_page_config(
 
 
 if check_password():
-    st.image("photos\ImageVariation_Logo.png")
+    st.image("photos/ImageVariation_Logo.png")
     st.write("""
     ### Instructions 🗈
 
