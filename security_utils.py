018bd4d2cb5c84794aa0dd6f7e2e8a1b9ba3ae20
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-08-21 15:54:05 +0800

    Update security_utils.py with User Activity Google API

--- security_utils.py
+++ security_utils.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
 
 introduction = """**IdeaSpark**   
 
@@ -9,24 +12,46 @@ Whether you're looking to boost engagement, increase conversions, or simply stan
 IdeaSpark is the perfect tool for any creative looking to take their social media advertising to the next level. 💡"""
 
 def password_entered():
-    """Checks whether a password entered by the user is correct."""
+    """Checks whether a username and password entered by the user are correct."""
 
-    if st.session_state["password"] == st.secrets["password"]: 
+    if st.session_state["username"] in st.secrets["users"] and st.session_state["password"] == st.secrets["users"][st.session_state["username"]]:
         st.session_state["password_correct"] = True
         del st.session_state["password"]  # don't store password
+
+        # Use credentials to create a client to interact with the Google Drive API
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('user-activity-ideaspark-aed98c90f4cb.json', scope)
+
+        activity = gspread.authorize(creds)
+
+        # Find a workbook by name and open the first sheet
+        sheet = activity.open("user-activity-ideaspark").sheet1
+
+        # Get the current date and time
+        now = datetime.now()
+        date_time = now.strftime("%m/%d/%Y %H:%M:%S")
+
+        # Insert data into the sheet
+        row = [st.session_state["username"], date_time]
+        index = 2
+        sheet.insert_row(row, index)
     else:
         st.session_state["password_correct"] = False
 
+
+
 #adding a login page
 def check_password():
-    """Returns `True` if the user had the correct password."""
+    """Returns `True` if the user had the correct username and password."""
 
     if "password_correct" not in st.session_state:
 
         st.header("IdeaSpark")
-        st.image("photos/IdeaSpark_Logo.png")  # Add this line to display the image
+        st.image("photos\IdeaSpark_Logo.png")  # Add this line to display the image
+        st.text_input("**Username**", key="username")
         st.text_input(
-        "Please enter your password", type="password", on_change=password_entered, key="password"
+            "**Password**", type="password", on_change=password_entered, key="password"
         )
         st.markdown(introduction)
         
@@ -34,10 +59,11 @@ def check_password():
     
     elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
+        st.text_input("**Username**", key="username")
         st.text_input(
-            "Please enter your password", type="password", on_change=password_entered, key="password"
+            "**Password**", type="password", on_change=password_entered, key="password"
         )
-        st.error("😕 Password incorrect")
+        st.error("😕 Username or password incorrect")
         return False
 
     else:
