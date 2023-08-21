f089061e0658a46cfdae437772aa34fe4804e04d
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-08-21 15:55:44 +0800

    Update 1_Open_Ended.py

--- 1_Open_Ended.py
+++ 1_Open_Ended.py
@@ -12,6 +12,17 @@ translator = Translator()
 # Access the OpenAI API key
 openai.api_key = st.secrets["api_key"]
 
+# Check if the "username" key exists in the st.session_state object
+if "username" not in st.session_state:
+    # If the key does not exist, initialize it with the default value "Invoke People"
+    st.session_state["username"] = "Invoke People"
+
+# Extract the name from the username
+name = st.session_state["username"].split('@')[0]
+
+# Capitalize the first letter of the name
+name = name.capitalize()
+
 # configure the default settings of the page.
 st.set_page_config(
                 page_title="IdeaSpark",
@@ -21,6 +32,9 @@ st.set_page_config(
                 )
 
 if check_password():
+    # Display a greeting message to the user
+    st.write(f'Hello, {name}!')
+  
     st.image("photos/OpenEnded_Logo.png")
     st.info(""" NOTE: You can download image by\
                     right clicking on the image and select save image as option""")  
