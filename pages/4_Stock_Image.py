9323ed807e00fdf066ec3dc3b6901463fac82d96
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-08-24 14:25:31 +0800

    Adjust attribute generated_images

--- pages/4_Stock_Image.py
+++ pages/4_Stock_Image.py
@@ -144,9 +144,13 @@ if check_password():
                 # Record user activity to Google Sheet
                 record_user_activity_stock_image(st.session_state["username"], st.secrets)
                 
+                # Check if the attribute generated_images exists in the st.session_state object
+                if "generated_images" not in st.session_state:
+                    # Initialize the attribute to an empty list if it does not exist
+                    st.session_state.generated_images = []
+                
                 # Save generated images to Google Drive
                 save_images_to_google_drive(st.session_state.generated_images, "IdeaSpark-Generated_Photo", st.secrets)
-                    
 
     # Create a button to show the history of generated images
     if st.button("Show History"):
